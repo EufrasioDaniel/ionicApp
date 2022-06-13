80aef96d52535ef2392b2babc6d8275e33e1a3c8
Eufrasio Daniel <edaniel@indracompany.com>
2022-06-13 11:40:29 -0300

    estudando ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public resultado: String = "";
+  public titulo: String = "Meu Primeiro APP";
 
-  constructor() {}
+  public imagemRandomica: String = "https://picsum.photos/800/600";
+  public imagemLocal: String = "../assets/icone-celular.png";
 
+  pesquisa: string;
+  nome: string;
+
+  constructor(private navegacao: NavController) { }
+
+  ngOnInit() {
+  }
+
+
+  recuperar(){
+    this.resultado = this.nome;
+  }
+
+  abrirBotoes(){
+    this.navegacao.navigateForward('botoes');
+  }
+
+  abrirLista(){
+    this.navegacao.navigateForward('lista');
+  }
+
+  public atualiza(): void{
+    this.titulo = "Texto Alterado";
+  }
+
+  public acao(): void{
+    this.titulo = "Botão clicado";
+  }
 }
